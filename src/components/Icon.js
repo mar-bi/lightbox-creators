bf56b5cae747f7416c7c77675835566f519c7891
mar-bi <marina.biletska@gmail.com>
2018-03-21 18:02:44 +0200

    add Icon component

---
+++ src/components/Icon.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Icon = ({ viewbox, size, color, icon}) => (
+  <svg
+    className="svg-icon"
+    viewBox={viewbox}
+    width={size}
+    height={size}
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <path d={icon} fill={color}/>
+  </svg>
+)
+
+export default Icon
+
+
+Icon.propTypes = {
+  icon: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  viewbox: PropTypes.string
+}
+
+Icon.defaultProps = {
+  size: 36,
+  viewbox: '0 0 32 32',
+  color: '#dcd0c0'
+}
