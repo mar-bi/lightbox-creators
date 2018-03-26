2aaa42eb6143ef7a66015890bfada9cdfbe24838
mar-bi <marina.biletska@gmail.com>
2018-03-26 11:53:07 +0200

    format blog entries of index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,42 +24,46 @@ export default class IndexPage extends React.Component {
         />
         <section className="section">
           <div className="container">
-            <div className="content">
-              <h2 className="has-text-weight-bold is-size-1" dir="rtl">
-                الأخبار
-              </h2>
-            </div>
-            {posts
-              .filter(post => post.node.frontmatter.templateKey === 'blog-post')
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #c0b283', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p dir="rtl" className="is-size-4">
-                    <Link className="has-text-danger" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small className="has-text-dark" dir="rtl">
-                      {rtlDate(post.frontmatter.date)}
-                    </small>
-                  </p>
-                  <p dir="rtl" className="is-size-5">
-                    {post.frontmatter.description}
-                    <br />
-                    <br />
-                    <Link
-                      className="button is-small is-danger is-outlined is-size-5 has-text-weight-semibold"
-                      to={post.fields.slug}
-                      dir="rtl"
-                    >
-                      استمرار
-                    </Link>
-                  </p>
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div className="content">
+                  <h2 className="has-text-weight-bold is-size-2" dir="rtl">
+                    الأخبار
+                  </h2>
                 </div>
-              ))}
+                {posts
+                  .filter(post => post.node.frontmatter.templateKey === 'blog-post')
+                  .map(({ node: post }) => (
+                    <div
+                      className="content blog-container"
+                      style={{ border: '1px solid #c0b283', padding: '2em 4em' }}
+                      key={post.id}
+                    >
+                      <p dir="rtl" className="is-size-3">
+                        <Link className="has-text-danger" to={post.fields.slug}>
+                          {post.frontmatter.title}
+                        </Link>
+                        <span> &bull; </span>
+                        <small className="has-text-dark" dir="rtl">
+                          {rtlDate(post.frontmatter.date)}
+                        </small>
+                      </p>
+                      <p dir="rtl" className="is-size-4">
+                        {post.frontmatter.description}
+                        <br />
+                        <br />
+                        <Link
+                          className="button is-small is-danger is-outlined is-size-5 has-text-weight-semibold"
+                          to={post.fields.slug}
+                          dir="rtl"
+                        >
+                          استمرار
+                        </Link>
+                      </p>
+                    </div>
+                  ))}
+              </div>
+            </div>
           </div>
         </section>
       </div>
